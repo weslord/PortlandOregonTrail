c08b2a18d1a68f1fc2b16111cb91b990fb86d026
Aaron Shaw <aaronshaw@Aarons-MacBook-Pro-2.local>
2016-11-05 13:08:09 -0700

    Added city images and links

--- data/cities.js
+++ data/cities.js
@@ -2,51 +2,61 @@ var cities = [
   {
     name: "Omaha",
     state: "Nebraska",
-    distanceRemaining: 1893
+    distanceRemaining: 1893,
+    img: "../omaha.jpg"
   },
   {
     name: "Lincoln",
     state: "Nebraska",
-    distanceRemaining: 1837
+    distanceRemaining: 1837,
+    img: "../lincoln.jpg"
   },
   {
     name: "Trenton",
     state: "Nebraska",
-    distanceRemaining: 1566
+    distanceRemaining: 1566,
+    img: "../trenton.jpg"
   },
   {
     name: "Boulder",
     state: "Colorado",
-    distanceRemaining: 1311
+    distanceRemaining: 1311,
+    img: "../boulder.jpg"
   },
   {
     name: "Grand Junction",
     state: "Colorado",
-    distanceRemaining: 1061
+    distanceRemaining: 1061,
+    img: "../grandjunction.jpg"
   },
   {
     name: "Salt Lake City",
     state: "Utah",
-    distanceRemaining: 777
+    distanceRemaining: 777,
+    img: "../saltlakecity.jpg"
   },
   {
     name: "Twin Falls",
     state: "Idaho",
-    distanceRemaining: 559
+    distanceRemaining: 559,
+    img: "../twinfalls.jpg"
   },
   {
     name: "Boise",
     state: "Idaho",
-    distanceRemaining: 431
+    distanceRemaining: 431,
+    img: "../boise.jpg"
   },
   {
     name: "Baker City",
     state: "Oregon",
-    distanceRemaining: 303
+    distanceRemaining: 303,
+    img: "../bakercity.jpg"
   },
   {
     name: "Portland",
     state: "Oregon",
-    distanceRemaining: 0
+    distanceRemaining: 0,
+    img: "../portland.jpg"
   }
 ]
