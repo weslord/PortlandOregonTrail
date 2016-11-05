e88fe8be54de9a23f704fd764ce46b7294590011
Kelvin Lau <kelvin.kl.lau@gmail.com>
2016-11-05 12:48:00 -0700

    Added randomEvents.js

---
+++ randomEvents.js
@@ -0,0 +1,42 @@
+var events = [
+	{
+		"description": "Flat tire, too bad man.",
+		"cost": -100,
+		"cool": 100 
+	},
+	{
+		"description": "Nobody liked your selfie on Facebook.",
+		"cost": 0,
+		"cool": -25
+	},
+	{
+		"description": "People have discovered you like Nickleback.",
+		"cost": 0,
+		"cool": -100
+	},
+	{
+		"description": "Trust fund invested in Silicon Valley app failure.",
+		"cost": -150,
+		"cool": 25
+	},
+	{
+		"description": "Bought an iPhone instead of an Android. No money for groceries.",
+		"cost": -1000,
+		"cool": 1000
+	},
+	{
+		"description": "Used parent's credit card to pay off maintenance fees.",
+		"cost": 200,
+		"cool": -200
+	},
+	{
+		"description": "Your buds found out you participated in the Hipster Olympics in 2006",
+		"cost": 0,
+		"cool": 100
+	},
+	{
+		"description": "Find vinyl record in shop while passing by.",
+		"cost": 100,
+		"cool": 100
+	}
+];
