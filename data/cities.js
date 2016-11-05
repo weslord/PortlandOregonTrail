7de87869483492ea18f1639601896360519191ed
Aaron Shaw <aaronshaw@Aarons-MacBook-Pro-2.local>
2016-11-05 12:37:20 -0700

    Cities list populated

---
+++ data/cities.js
@@ -0,0 +1,52 @@
+var cities = [
+  {
+    name: "Omaha",
+    distance: 0,
+    distanceRemaining: 1893
+  },
+  {
+    name: "Lincoln",
+    distance: 56,
+    distanceRemaining: 1837
+  },
+  {
+    name: "Trenton",
+    distance: 271,
+    distanceRemaining: 1566
+  },
+  {
+    name: "Boulder",
+    distance: 255,
+    distanceRemaining: 1311
+  },
+  {
+    name: "Grand Junction",
+    distance: 250,
+    distanceRemaining: 1061
+  },
+  {
+    name: "Salt Lake City",
+    distance: 284,
+    distanceRemaining: 777
+  },
+  {
+    name: "Twin Falls",
+    distance: 218,
+    distanceRemaining: 559
+  },
+  {
+    name: "Boise",
+    distance: 128,
+    distanceRemaining: 431
+  },
+  {
+    name: "Baker City",
+    distance: 128,
+    distanceRemaining: 303
+  },
+  {
+    name: "Portland",
+    distance: 303,
+    distanceRemaining: 0
+  }
+]
