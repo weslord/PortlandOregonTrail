c5a11e523e6d0978889d3ef26c50fafb22070a81
maxahn <ahnmaximus@gmail.com>
2016-11-05 16:00:40 -0700

    moving cities

--- js/index.js
+++ js/index.js
@@ -1,8 +1,41 @@
 $(document).ready(function() {
-  var currentCity = 0;
   var imageInterval = 0;
-  $('#currentCityName').text(cities[currentCity].name);
-  $('#milesToGoNum').text(cities[currentCity].distanceRemaining);
+
+  var currentCity;
+  var mileage; //will be on instance of car
+  const TOTALMILES = 1893;
+
+  var cityOptions = [];
+
+  function setUpStart() {
+    mileage = 0;
+    currentCity = 0;
+    $('#currentCityName').text(cities[0].name);
+    $('#milesTravelledNum').text(mileage);
+    $('#milesToGoNum').text(TOTALMILES);   
+    $('<p> <span id="goWest">GO WEST</span></p>').appendTo('.choices');
+  }
+
+  function goWest() {
+    (currentCity == undefined) ? currentCity = 0 : currentCity++;
+    if (currentCity < cities.length) {
+      if (currentCity == 0) {
+        mileage = TOTALMILES - cities[currentCity].distanceRemaining;
+      } else {
+        mileage += cities[currentCity - 1].distanceRemaining - cities[currentCity].distanceRemaining;
+      }
+
+      $('#currentCityName').text(cities[currentCity].name);
+      $('#milesTravelledNum').text(mileage);   
+    } else {
+      $('#currentCityName').text(cities[cities.length - 1].name);
+      $('#milesToGoNum').text(0);   
+    }
+  }
+
+  setUpStart();
+
+  $('#goWest').on('click', goWest); 
 
   setInterval(function(){
       imageInterval+=1;
