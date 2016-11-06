5492ef43b29a46102d79b678cf2595db161a7fec
weslord <weslord@gmail.com>
2016-11-06 10:50:19 -0800

    Better UI for notifications

--- js/notification.js
+++ js/notification.js
@@ -1,27 +1,32 @@
+$('#notification').click(function(){
+  $('#notification').hide();
+  $('#notification-cool').css('color', 'white');
+  $('#notification-cost').css('color', 'white');
+  $('#notification').css('top', '100%');
+});
+
 function notification(text, cool, cost) {
   $('#notification-text').text(text);
   if (cool > 0) {
     $('#notification-text').text(text);
-    $('#notification-cool').text('Gained ' + cool + ' cool points!')
+    $('#notification-cool').text('Gained ' + cool + '% coolness!').css('color', 'lime');
   } else if (cool < 0) {
     var coolString = cool.toString();
     // $('#notification-cool').addClass('loss');
-    $('#notification-cool').text('Lost ' + coolString.substring(1, coolString.length) + ' cool points!').css('color', 'red');
+    $('#notification-cool').text('Lost ' + coolString.substring(1, coolString.length) + '% coolness!').css('color', 'red');
   }
   if (cost > 0) {
-    $('#notification-cost').text('Gained ' + cost + ' dollars!')
+    $('#notification-cost').text('Gained $' + cost + '!').css('color', 'lime');
 
   } else if (cost < 0) {
     var costString = cost.toString();
     // $('#notification-cost').addClass('loss');
-    $('#notification-cost').text('Lost ' + costString.substring(1, costString.length) + ' dollars!').css('color', 'red');
+    $('#notification-cost').text('Lost $' + costString.substring(1, costString.length) + '!').css('color', 'red');
   }
+  
+  $('#notification').show();
+  $('#notification').animate({top:'0px'},1000);
 
-  $('#notification').show();//.text();
   $('hi').appendTo('#notification');
-  $('#notification').click(function(){
-    $('#notification').hide();
-    $('#notification-cool').css('color', 'white');
-    $('#notification-cost').css('color', 'white');
-  });
+
 }
